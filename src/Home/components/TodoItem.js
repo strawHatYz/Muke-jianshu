497c875d87340f72318c3097cd3063ff8b42fb01
starwHatYz <1013951740@qq.com>
2020-08-18 19:26:11 +0800

    Merge 声明周期

--- src/Home/components/TodoItem.js
+++ src/Home/components/TodoItem.js
@@ -1,10 +1,13 @@
 import React,{Component} from 'react'
+import PropTypes from 'prop-types'
  
 class TodoItem extends Component {
   constructor(props) {
     super(props)
+    // 当父组件的render执行时，子组件render会重新执行一次
     this.handleClick = this.handleClick.bind(this)
   }
+  // 当组件离得state或props发生改变,render函数就会重新执行
   render () {
     let {content} = this.props
     return (
@@ -18,5 +21,14 @@ class TodoItem extends Component {
     deleteItem(index)
     
   }
+}
+// Prop Types 作为类型验证，验证父组件传值类型是否正确。
+TodoItem.propTypes = {
+  content :PropTypes.string,
+  deleteItem: PropTypes.func,
+  index:PropTypes.number.isRequired // isRequired 代表必须传递
+}
+TodoItem.defaultProps = {
+  content:'你好'
 }
 export default TodoItem
--- src/Home/index.js
+++ src/Home/index.js
@@ -6,6 +6,7 @@ class TodoList extends Component {
   // 最优先执行constyroctor
   constructor(props) {
     super(props);
+    // 当组件的statr或props发生变化的时候，render函数就会重新执  行。
     this.state = {
       inputValue: '',
       list: [],
@@ -14,13 +15,20 @@ class TodoList extends Component {
     this.handleInputChange = this.handleInputChange.bind(this)
     this.handleItemDelete = this.handleItemDelete.bind(this)
   }
+  // 首先执行componentWillMount>render>componentDidMount
+  // 页面挂在的时候执行
+  componentWillMount(){
+    console.log('componentWillMount,当组件即将被挂载到页面执行')
+  }
+  // 当组件离得state或props发生改变,render函数就会重新执行
   render() {
+    console.log('render')
     return (
       <Fragment>
         <div>
           {/* 多行注释 */}
           <label htmlFor="inserArea">输入内容</label>
-          <input id="inserArea" className="input" value={this.state.inputValue} onChange={this.handleInputChange} />
+          <input id="inserArea" className="input" ref={(input)=>{this.input=input}} value={this.state.inputValue} onChange={this.handleInputChange} />
           <button onClick={this.handleButtonClick}>提交</button>
         </div>
         <ul>
@@ -28,6 +36,26 @@ class TodoList extends Component {
         </ul>
       </Fragment>
     )
+  }
+  // 组件被加载后自动执行
+  componentDidMount(){
+    console.log('componentDidMount,组件被加载后自动执行')
+  }
+  // 组件更新
+  shouldComponentUpdate(){
+    
+    console.log('shouldComponentUpdate,组件更新之前,会被自动执行')
+    //shouldComponentUpdate声明周期要求返回Boolean类型的结果，意思是你的组件是否需要更新？如果返回false不会更新组件，页面也就会产生不同的反馈。
+    // 如果返回false就不会继续声明周期
+    return true // 继续更新组件
+  }
+  // 组件被更新之前会自动执行。但是他在shouldComponentUpdate之后被执行,如果shouldComponentUpdate返回true执行,如果返回false就不会执行了。
+  componentWillUpdate(){
+    console.log('componentWillUpdate','组件被更新之前会自动执行。但是他在shouldComponentUpdate之后被执行')
+  }
+  
+  componentDidUpdate(){
+    console.log('componentDidUpdate','页面重新渲染后执行')
   }
   getDodoItem() {
     return this.state.list.map((item, index) => {
@@ -43,6 +71,7 @@ class TodoList extends Component {
     // 使用serState改变
     // this.setState({
     //   inputValue: e.target.value
+    // e.target为DOM元素
     // }) // 新版不推荐变更，推荐使用函数,变成异步数据设置
     const value = e.target.value
     this.setState(() => ({ // es6返回对象直接写小括号
